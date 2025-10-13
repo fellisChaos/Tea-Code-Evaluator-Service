bf10dde0b28afe0b28434cbacfe4435e386f30cc
fellisChaos <nishantubuntu@gmail.com>
2025-10-13 23:45:38 +0530

    Basic TS + Express project setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { PORT } from "./config/server.config";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
