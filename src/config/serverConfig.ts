64e807192eda43217f5c056b9e98fab12a94de7f
fellisChaos <nishantubuntu@gmail.com>
2025-10-15 19:55:11 +0530

    updated ioredis file config

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
-export const REDIS_PORT = process.env.REDIS_PORT || '6379';
+export const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379');
